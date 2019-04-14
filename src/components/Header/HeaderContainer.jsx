ab5d35b6d01fb315fae68a117f3f366292c1f9c9
Avdeev Denis <av-denis@yandex-team.ru>
2019-04-15 00:25:04 +0300

    overlay test

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import burgerMenuIcon from '../../assets/images/burger-menu.svg';
+import crossIcon from '../../assets/images/cross.svg';
+
 import { getCookie, firstUpper } from '../../tools/index'
 
 import './index.scss'
@@ -13,6 +16,22 @@ export default class HeaderContainer extends React.Component {
         super(props);
 
         this.onLogoutClick = this.onLogoutClick.bind(this);
+        this.onClickMenu = this.onClickMenu.bind(this);
+        this.onSidebarClick = this.onSidebarClick.bind(this);
+
+        this.state = '';
+    }
+
+    onClickMenu() {
+        this.setState({
+            isOpenedSidebar: !this.state.isOpenedSidebar
+        });
+    }
+
+    onSidebarClick() {
+        this.setState({
+            isOpenedSidebar: !this.state.isOpenedSidebar
+        });
     }
 
     onLogoutClick() {
@@ -33,7 +52,10 @@ export default class HeaderContainer extends React.Component {
         return (
             <div className='header'>
                 <div className='header__logo'>
-                    Hello, <span className='header__login'>{firstUpper(this.props.login)}</span>
+                    <img className='header__burger' src={burgerMenuIcon} onClick={this.onClickMenu} alt='' />
+                    <p>
+                        Hello, <span className='header__login'>{firstUpper(this.props.login)}</span>
+                    </p>
                 </div>
                 <div className='header__logout'>
                     <Link to='' onClick={this.onLogoutClick}>
@@ -42,6 +64,31 @@ export default class HeaderContainer extends React.Component {
                         </Button>
                     </Link>
                 </div>
+                <div className={'overlay ' + (this.state.isOpenedSidebar ? ' overlay_opened' : '')} onClick={this.onSidebarClick}></div>
+                {<div className={'sidebar ' + (this.state.isOpenedSidebar ? 'sidebar_opened' : '')}>
+                    <div className='sidebar__header'>
+                        <img className='sidebar__close' src={crossIcon} onClick={this.onSidebarClick} />
+                    </div>
+                    <div className='sidebar__content'>
+                        <div className='sidebar__top'>
+                            <Button block variant='success'>
+                                Друзья
+                            </Button>
+                            <Button block variant='success'>
+                                Сообщения
+                            </Button>
+                            <Button block variant='success'>
+                                Пользователи
+                            </Button>
+                        </div>
+
+                        <div className='sidebar__bottom'>
+                            <Button block variant='info'>
+                                Выход
+                        </Button>
+                        </div>
+                    </div>
+                </div>}
             </div>
         );
     }
