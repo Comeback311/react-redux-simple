676a914311ef45ae98f49707ea513bb2881899bf
Avdeev Denis <av-denis@yandex-team.ru>
2019-04-14 16:56:06 +0300

    some data

---
+++ server/secret/db.js
@@ -0,0 +1,6 @@
+const user = 'avdeev_denis';
+const password = encodeURIComponent('f7nU3#4w3KRgWmG7');
+
+const url = 'mongodb+srv://' + user + ':' + password + '@cluster0-vqy1a.mongodb.net/test?retryWrites=true';
+
+export default url;
