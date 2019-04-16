4024769571b9a99b5e34f3f7be766f666af177b3
Avdeev Denis <av-denis@yandex-team.ru>
2019-04-16 11:48:12 +0300

    swipe to open sideblock

--- src/components/Overlay/OverlayContainer.jsx
+++ src/components/Overlay/OverlayContainer.jsx
@@ -7,6 +7,11 @@ export default class OverlayContainer extends React.Component {
         super(props);
 
         this.onOverlayClick = this.onOverlayClick.bind(this);
+        this.onTouchEnd = this.onTouchEnd.bind(this);
+        this.onTouchStart = this.onTouchStart.bind(this);
+
+        this.initialPoint = null;
+        this.finalPoint = null;
     }
 
     onOverlayClick(e) {
@@ -15,11 +20,32 @@ export default class OverlayContainer extends React.Component {
         this.props.toggleSidebar();
     }
 
+    onTouchStart(e) {
+        this.initialPoint = e.changedTouches[0];
+    }
+
+    onTouchEnd(e) {
+        this.finalPoint = e.changedTouches[0];
+
+        if (this.needToDoSwipeLeft()) {
+            this.props.toggleSidebar();
+        }
+    }
+
+    needToDoSwipeLeft() {
+        const xAbs = Math.abs(this.initialPoint.pageX - this.finalPoint.pageX);
+        const yAbs = Math.abs(this.initialPoint.pageY - this.finalPoint.pageY);
+
+        return xAbs > 20 || yAbs > 20 && xAbs > yAbs && this.finalPoint.pageX < this.initialPoint.pageX;
+    }
+
     render() {
         return (
-            <div 
-                className={'overlay ' + (this.props.isOpenedSidebar ? ' overlay_opened' : '')} 
+            <div
+                className={'overlay ' + (this.props.isOpenedSidebar ? ' overlay_opened' : '')}
                 onClick={this.onOverlayClick}
+                onTouchEnd={this.onTouchEnd}
+                onTouchStart={this.onTouchStart}
             ></div>
         );
     }
